414f5b75c48b4062e34ec85525ce3e78594ca2e9
Aller Augusto de Souza <allerasouza@gmail.com>
2022-11-23 06:41:44 -0300

    feat: add day 5 source code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
-// Declaring all variables
- 
-let code: number = 72;
-console.log(code);
+class Users {
+    name: string;
+    public balance: number = 0 ;
+
+    constructor(n: string, b: number) {
+        this.name = n;
+        this.balance = b;
+    }
+
+    addMoney(amount: number) {
+        this.balance += amount;
+    }
+}
+
+const user1 = new Users('Andre', 10);
+user1.addMoney(100);
+
+const user2 = new Users('Marcos', 20);
+user2.addMoney(30);
+
+console.log(user1);
+console.log(user2);
